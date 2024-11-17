c1f6b26a081149bab54e5474f6b55b6224dae5d6
Johann Feser <johannfeser@gmail.com>
2024-11-17 19:11:39 +0100

    Update MobileMain.tsx

--- src/components/mobile/MobileMain.tsx
+++ src/components/mobile/MobileMain.tsx
@@ -3,6 +3,7 @@ import {DonationButton} from "../common/DonationButton.tsx";
 import {Tabs} from "@kobalte/core/tabs";
 import {NavBar} from "../common/NavBar.tsx";
 import {ExtensionTabs} from "../common/ExtensionTabs.tsx";
+import {PanelHeader} from "../common/PanelHeader.tsx";
 
 
 export const MobileMain: Component = (props) => {
@@ -10,7 +11,7 @@ export const MobileMain: Component = (props) => {
     <Tabs
       class={'h-screen flex-1 flex flex-col gap-2 py-2'}
     >
-      <DonationButton/>
+      <PanelHeader/>
       <ExtensionTabs/>
       <NavBar/>
     </Tabs>
