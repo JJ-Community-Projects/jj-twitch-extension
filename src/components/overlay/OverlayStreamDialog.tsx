65d5c1d4cff24f67381d538b70bd7afa72e39c74
Johann Feser <johannfeser@gmail.com>
2024-11-17 19:12:42 +0100

    Update OverlayStreamDialog.tsx

--- src/components/overlay/OverlayStreamDialog.tsx
+++ src/components/overlay/OverlayStreamDialog.tsx
@@ -1,11 +1,12 @@
 import type {TESTwitchCreator, TESTwitchLink} from "../../lib/model/TwitchExtensionSchedule.ts";
-import {type Component, For, Show} from "solid-js";
+import {type Component, For, Match, Show, Switch} from "solid-js";
 import {DateTime} from "luxon";
 import {FaBrandsTwitch} from "solid-icons/fa";
 import {useOverlay} from "../common/providers/OverlayProvider.tsx";
 import {AiOutlineClose} from "solid-icons/ai";
 import {YogsStreamUtils} from "../../lib/YogsStreamUtils.ts";
 import {useNow} from "../../lib/useNow.ts";
+import {getTextColor} from "../../lib/textColors.ts";
 
 
 export const OverlayStreamDialog: Component = (props) => {
@@ -109,15 +110,48 @@ interface CreatorComponentProps {
 }
 
 const CreatorComponent: Component<CreatorComponentProps> = (props) => {
+  const url = () => props.creator.url
+
+  const color = () => {
+    return props.creator.color
+  }
+
+  const hasUrl = () => {
+    return props.creator.url !== ''
+  }
+
+  const label = () => props.creator.label
+
   return (
-    <div class={'flex flex-row py-1'}>
-      <a
-        class={'hover:scale-102 text-xxs flex flex-row items-center p-2 rounded-2xl bg-twitch-500 text-white transition-all '}
-        target={'_blank'}
-        href={props.creator.url}
-      >
-        <FaBrandsTwitch/>/{props.creator.label}
-      </a>
-    </div>
+    <Switch>
+      <Match when={hasUrl()}>
+        <div class={'flex flex-row py-1'}>
+          <a
+            class={'hover:scale-102 text-xxs flex flex-row items-center p-2 rounded-2xl transition-all'}
+            style={{
+              background: color(),
+              color: getTextColor(color())
+            }}
+            target={'_blank'}
+            href={url()}
+          >
+            {label()}
+          </a>
+        </div>
+      </Match>
+      <Match when={!hasUrl()}>
+        <div class={'flex flex-row py-1'}>
+          <p
+            class={'text-xxs flex flex-row items-center p-2 rounded-2xl'}
+            style={{
+              background: color(),
+              color: getTextColor(color())
+            }}
+          >
+            {label()}
+          </p>
+        </div>
+      </Match>
+    </Switch>
   );
 }
