dfbadbb2bcf558033708490c96a40535554d3164
Johann Feser <johannfeser@gmail.com>
2024-11-17 19:12:45 +0100

    Update PanelMain.tsx

--- src/components/panel/PanelMain.tsx
+++ src/components/panel/PanelMain.tsx
@@ -3,11 +3,12 @@ import {DonationButton} from "../common/DonationButton.tsx";
 import {Tabs} from "@kobalte/core/tabs";
 import {NavBar} from "../common/NavBar.tsx";
 import {ExtensionTabs} from "../common/ExtensionTabs.tsx";
+import {PanelHeader} from "../common/PanelHeader.tsx";
 
 export const PanelMain: Component = (props) => {
   return (
     <Tabs class={'h-screen flex-1 flex flex-col gap-2 py-2'}>
-      <DonationButton/>
+      <PanelHeader/>
       <NavBar/>
       <ExtensionTabs/>
     </Tabs>
