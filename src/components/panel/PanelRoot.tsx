b361df1c6385742a6bd8854e51403c3beec504d0
Johann Feser <johannfeser@gmail.com>
2025-10-30 21:43:02 +0100

    add tanstack query dev tools

--- src/components/panel/PanelRoot.tsx
+++ src/components/panel/PanelRoot.tsx
@@ -2,6 +2,7 @@ import {type Component, lazy} from "solid-js";
 import {PanelMain} from "./PanelMain.tsx";
 import {Background} from "../common/Background.tsx";
 import {PanelExtensionProviders} from "../common/providers/PanelExtensionProviders.tsx";
+import {SolidQueryDevtools} from "@tanstack/solid-query-devtools";
 
 
 // const PanelMain = lazy(() => import('./PanelMain.tsx'));
@@ -12,6 +13,7 @@ export const PanelRoot: Component = () => {
       <Background>
         <PanelMain/>
       </Background>
+      <SolidQueryDevtools initialIsOpen={false}/>
     </PanelExtensionProviders>
   );
 }
