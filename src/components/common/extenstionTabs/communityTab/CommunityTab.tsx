dd2244062ff48f08dbeff2eddece01f80a99f89d
Johann Feser <johannfeser@gmail.com>
2025-10-30 21:44:38 +0100

    Update CommunityTab.tsx

--- src/components/common/extenstionTabs/communityTab/CommunityTab.tsx
+++ src/components/common/extenstionTabs/communityTab/CommunityTab.tsx
@@ -5,16 +5,20 @@ import {useTheme} from "../../providers/ThemeProvider.tsx";
 import {twMerge} from "tailwind-merge";
 import {Numeric} from "solid-i18n";
 import {FiExternalLink} from "solid-icons/fi";
-import {InvisibleBody} from "../../InvisibleBody.tsx";
+import {InvisibleBody, InvisibleBodyAlt} from "../../InvisibleBody.tsx";
 import {useBackend} from "../../providers/BackendProvider.tsx";
 import type {CurrenciesSchema, JJCampaign} from "../../../../api";
 import {TwitchIcon} from "../../icons/JJIcons.tsx";
 import {FaBrandsTwitch} from "solid-icons/fa";
+import {CommunityListItemAlt, CommunityListItem} from "./CommunityListItem.tsx";
+import {CurrencyToggle} from "../../CurrencyToggle.tsx";
+import {CrossFade} from "../../CrossFade.tsx";
+import {Loading} from "../../Loading.tsx";
+import {ErrorPage} from "../../Error.tsx";
 
 
 export const CommunityTab: Component = () => {
   const {config} = useBackend()
-
 
   return (
     <Show when={config.data}>
@@ -22,9 +26,9 @@ export const CommunityTab: Component = () => {
         (config) => {
           return (
             <Show when={config().showFundraisers} fallback={
-              <InvisibleBody text={'The Community Fundraisers will be shown soon after the Jingle Jam has started.'}>
+              <InvisibleBodyAlt text={'The Community Fundraisers will be shown soon after the Jingle Jam has started.'}>
                 <JJStreamTeamLink/>
-              </InvisibleBody>
+              </InvisibleBodyAlt>
             }>
               <Body/>
             </Show>
@@ -39,26 +43,35 @@ export const CommunityTab: Component = () => {
 const Body = () => {
   const {campaigns} = useBackend()
 
-
   return (
-    <Show when={campaigns.data}>
-      {
-        (campaigns) => {
-          return (
-            <div class={'flex h-full flex-1 flex-col'}>
-              <ColoredScrollbar>
-                <div class={'flex flex-1 flex-col gap-2 mx-2'}>
-                  <FundraiserBody fundraisers={campaigns().campaigns}/>
+    <>
+      <CrossFade show={campaigns.isError}>
+        <ErrorPage message={'Failed to load fundraisers.'}/>
+      </CrossFade>
+      <CrossFade show={campaigns.isPending}>
+        <Loading/>
+      </CrossFade>
+      <CrossFade show={campaigns.data !== undefined}>
+        <Show when={campaigns.data}>
+          {
+            (campaigns) => {
+              return (
+                <div class={'flex h-full flex-1 flex-col'}>
+                  <ColoredScrollbar>
+                    <div class={'flex flex-1 flex-col gap-2 mx-2'}>
+                      <FundraiserBody fundraisers={campaigns().campaigns}/>
+                    </div>
+                  </ColoredScrollbar>
+                  <p class={'text-center text-xxs text-black bg-white rounded-full p-1 mt-1 mx-2'}>
+                    Last update, {DateTime.fromJSDate(campaigns().date).toLocaleString(DateTime.DATETIME_MED)}
+                  </p>
                 </div>
-              </ColoredScrollbar>
-              <p class={'text-center text-xxs text-black bg-white rounded-full p-1 mt-1 mx-2'}>
-                Last update, {DateTime.fromJSDate(campaigns().date).toLocaleString(DateTime.DATETIME_MED)}
-              </p>
-            </div>
-          )
-        }
-      }
-    </Show>
+              )
+            }
+          }
+        </Show>
+      </CrossFade>
+    </>
   )
 }
 
@@ -68,37 +81,15 @@ const FundraiserBody: Component<{ fundraisers: JJCampaign[] }> = props => {
 
   return (
     <>
-      <JJStreamTeamLink/>
+      <TopBar/>
       <For each={fundraiser()} fallback={
         <p class={'text-center text-white'}>No Fundraisers found.</p>
       }>
-        {(d, i) => {
-          const isTwitch = () => d.twitch !== undefined
-          const img = () => d.twitch?.avatar ?? d.avatar
-
-
-          const isLive = () => {
-            if (d.twitch) {
-              return d.twitch.isLive
-            }
-            return false
-          }
-
-          const name = () => {
-            return d.twitch?.name ?? d.tiltifyName
-          }
-
+        {(c, i) => {
           return (
-            <Child
+            <CommunityListItemAlt
               i={i()}
-              img={img()}
-              title={d.campaignName}
-              subtitle={name()}
-              desc={d.tiltifyDescription ?? ''}
-              isLive={isLive()}
-              raised={d.raised}
-              tiltifyUrl={d.tiltifyUrl}
-              twitchUrl={d.twitch?.url}
+              campaign={c}
             />
           )
         }}
@@ -107,100 +98,27 @@ const FundraiserBody: Component<{ fundraisers: JJCampaign[] }> = props => {
   )
 }
 
-const Live = () => {
-  return (
-    <div class={'relative flex h-3 items-center justify-center'}>
-      <p class={'bg-twitch-500 rounded p-0.5 text-[6px] text-white'}>LIVE</p>
-    </div>
-  )
-}
-
-const Child: Component<{
-  i: number
-  title: string
-  subtitle: string
-  img: string
-  isLive: boolean
-  desc: string
-  raised: CurrenciesSchema
-  tiltifyUrl: string
-  twitchUrl?: string
-}> = props => {
-  const {theme, tailwindTextPrimary} = useTheme()
-
-  const raisedColor = () => {
-    if (theme() === 'rainbow') {
-      return 'text-black'
-    } else if (theme() === 'dark') {
-      return 'text-white'
-    }
-    return tailwindTextPrimary()
-  }
-
-  const gradient = [
-    'bg-gradient-to-br from-red-200 to-red-400',
-    'bg-gradient-to-br from-orange-200 to-orange-400',
-    'bg-gradient-to-br from-yellow-200 to-yellow-400',
-    'bg-gradient-to-br from-green-200 to-green-400',
-    'bg-gradient-to-br from-cyan-200 to-cyan-400',
-    'bg-gradient-to-br from-blue-200 to-blue-400',
-    'bg-gradient-to-br from-purple-200 to-purple-400',
-  ]
-
-  const campaignColor = (i: number) => {
-    if (theme() === 'rainbow') {
-      return gradient[i % gradient.length]
-    } else if (theme() === 'dark') {
-      return 'bg-gray-500 text-white'
-    }
-    return 'bg-gradient-to-br from-white to-gray-100'
-  }
-  return (
-    <div class={twMerge('min-h-24 w-full rounded-2xl shadow-xl', campaignColor(props.i))}>
-      <div class={'flex h-full w-full flex-col items-start gap-2 p-1.5'}>
-        <div class={'flex h-full flex-1 flex-col gap-1'}>
-          <div class={'flex flex-row gap-1'}>
-            <img class={'h-8 w-8 rounded-lg'} alt={props.title} src={props.img} loading={'lazy'}/>
-            <div class={'flex h-full flex-1 min-w-0 flex-col justify-between'}>
-              <div class={'flex max-h-[14px] flex-row items-center gap-1 overflow-hidden'}>
-                <Show when={props.isLive}>
-                  <Live/>
-                </Show>
-                <p class={'truncate text-ellipsis text-sm font-bold min-w-0 flex-1'}>{props.title}</p>
-              </div>
-              <p class={'truncate text-ellipsis text-xs font-bold min-w-0'}>{props.subtitle}</p>
-            </div>
-          </div>
-          <p class={'line-clamp-2 w-full text-ellipsis text-xs'}>{props.desc}</p>
-          <p class={twMerge('text-primary text-xs font-bold', raisedColor())}>
-            Raised <Numeric value={props.raised.gbp} numberStyle="currency" currency={'GBP'}/>
-          </p>
-        </div>
-
-        <div class={'flex flex-row gap-2'}>
-          <a target={'_blank'} href={props.tiltifyUrl}
-             class={'transition-all hover:scale-102 hover:brightness-105 text-white gap-1 rounded-full bg-tiltify-500 p-1 flex flex-row items-center justify-center'}>
-            <span class={'text-xxs'}>Donate</span> <FiExternalLink size={12}/>
-          </a>
-          <Show when={props.twitchUrl}>
-            <a target={'_blank'} href={props.twitchUrl}
-               class={'transition-all hover:scale-102 hover:brightness-105 text-white gap-1 rounded-full bg-twitch-500 p-1 flex flex-row items-center justify-center'}>
-              <span class={'text-xxs'}>Twitch</span> <FiExternalLink size={12}/>
-            </a>
-          </Show>
-        </div>
-      </div>
-    </div>
-  )
-}
-
 const JJStreamTeamLink = () => {
   return (
     <a
       href={'https://twitch.tv/team/jinglejam'}
       target={'_blank'}
-      class={'flex flex-row justify-between items-center p-2 rounded-2xl text-white bg-twitch text-center hover:scale-101 hover:brightness-105 transition-all'}>Jingle
+      class={'flex flex-row gap-2 justify-between items-center p-2 rounded-2xl text-white bg-accent text-center hover:scale-101 hover:brightness-105 transition-all'}>Jingle
       Jam Stream Team
       <FiExternalLink/></a>
+  )
+}
+
+
+const TopBar: Component = () => {
+  return (
+    <div class={'flex flex-row justify-between bg-white p-2 rounded-2xl items-start'}>
+      <a
+        href={'https://twitch.tv/team/jinglejam'}
+        target={'_blank'}
+        class={'text-xs gap-2 flex flex-row justify-between items-center p-2 rounded-2xl text-white bg-accent text-center hover:scale-101 hover:brightness-101 transition-all'}>Jingle
+        Jam Stream Team <FiExternalLink/></a>
+      <CurrencyToggle/>
+    </div>
   )
 }
