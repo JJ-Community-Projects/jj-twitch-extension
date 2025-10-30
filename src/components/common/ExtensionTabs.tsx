2803f56aebbc7dc9713da4e2783d21dee6d1953e
Johann Feser <johannfeser@gmail.com>
2025-10-30 21:44:05 +0100

    Update ExtensionTabs.tsx

--- src/components/common/ExtensionTabs.tsx
+++ src/components/common/ExtensionTabs.tsx
@@ -1,40 +1,17 @@
-import {type Component, For, Show, createEffect, type ParentComponent} from "solid-js";
+import {type Component, For, Show} from "solid-js";
 import {JJTab} from "./JJTabs.tsx";
 import {useBackend} from "./providers/BackendProvider.tsx";
-import {useTabs} from "./TabsProvider.tsx";
-
-const CrossFade: ParentComponent<{ show: boolean }> = (props) => {
-  return (
-    <div
-      class={`absolute inset-0 transition-opacity duration-150 ease-linear ${props.show ? 'opacity-100 pointer-events-auto' : 'opacity-0 pointer-events-none'}`}
-    >
-      {props.children}
-    </div>
-  )
-}
 
 export const ExtensionTabs: Component = () => {
   const {userConfig} = useBackend()
-  const {currentTab, setCurrentTab} = useTabs()
-
   return (
     <Show when={userConfig.data}>
       {(config) => {
-        // initialize the current tab to the first available if not set yet
-        createEffect(() => {
-          const tabs = config().tabs
-          if (tabs.length > 0 && !currentTab()) {
-            setCurrentTab(tabs[0])
-          }
-        })
-
         return (
           <div class={'relative h-full w-full flex-1 overflow-hidden'}>
             <For each={config().tabs}>
               {(tab) => (
-                <CrossFade show={currentTab() === tab}>
-                  <JJTab tab={tab} bare/>
-                </CrossFade>
+                <JJTab tab={tab}/>
               )}
             </For>
           </div>
