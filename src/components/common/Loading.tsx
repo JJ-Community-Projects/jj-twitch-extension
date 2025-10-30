5e970842cf5b1ea11821c3e708fd641c173b2208
Johann Feser <johannfeser@gmail.com>
2025-10-30 21:43:36 +0100

    Create Loading.tsx

---
+++ src/components/common/Loading.tsx
@@ -0,0 +1,19 @@
+import type {Component} from "solid-js";
+import JingleJamLogo from "../../assets/JingleJam_Red.png";
+
+export const Loading: Component = () => {
+  return (
+    <div class="h-full w-full flex items-center justify-center">
+      <div class="flex flex-col items-center gap-6">
+        <img
+          src={JingleJamLogo.src}
+          alt="Jingle Jam"
+          class="w-40 md:w-56 select-none pointer-events-none drop-shadow-lg animate-pulse"
+        />
+        <div class="flex items-center gap-3 text-white">
+          <span class="text-sm">Loading…</span>
+        </div>
+      </div>
+    </div>
+  );
+};
