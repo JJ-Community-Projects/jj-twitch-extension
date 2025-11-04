c1f32b2b6e4bdf213265b83e9ab224145617d047
Johann Feser <johannfeser@gmail.com>
2025-11-04 17:16:59 +0100

    Update MobileMain.tsx

--- src/components/mobile/MobileMain.tsx
+++ src/components/mobile/MobileMain.tsx
@@ -1,18 +1,18 @@
 import {type Component} from "solid-js";
 import {Tabs} from "@kobalte/core/tabs";
-import {NavBar} from "../common/NavBar.tsx";
 import {ExtensionTabs} from "../common/ExtensionTabs.tsx";
 import {PanelHeader} from "../common/PanelHeader.tsx";
+import {NavBarAlt} from "../common/NavBarAlt.tsx";
 
 
-export const MobileMain: Component = (props) => {
+export const MobileMain: Component = () => {
   return (
     <Tabs
       class={'h-screen flex-1 flex flex-col gap-2 py-2'}
     >
       <PanelHeader/>
       <ExtensionTabs/>
-      <NavBar/>
+      <NavBarAlt/>
     </Tabs>
   );
 }
