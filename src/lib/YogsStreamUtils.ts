6e5ecd1d1f9461766ca58f54c3e2330d759a8afc
Johann Feser <johannfeser@gmail.com>
2025-12-04 15:18:43 +0100

    Update YogsStreamUtils.ts

--- src/lib/YogsStreamUtils.ts
+++ src/lib/YogsStreamUtils.ts
@@ -19,54 +19,29 @@ export class YogsStreamUtils {
   }
 
   static end(slot: Stream) {
-    return YogsStreamUtils.start(slot).plus(YogsStreamUtils.duration(slot))
-  }
-
-  static sortByNextStream(a: Stream, b: Stream, now?: DateTime) {
-    if (YogsStreamUtils.isLive(a, now) && !YogsStreamUtils.isLive(b, now)) {
-      return -1
-    }
-    if (YogsStreamUtils.isLive(b, now) && !YogsStreamUtils.isLive(a, now)) {
-      return 1
-    }
-
-    const startA = YogsStreamUtils.nextStream(a, now)
-    const startB = YogsStreamUtils.nextStream(b, now)
-    return startA.diff(startB).as('second')
-  }
-
-  static nextStream(slot: Stream, now?: DateTime) {
-    if (!now) {
-      now = DateTime.now()
-    }
-    const start = YogsStreamUtils.start(slot)
-    const duration = YogsStreamUtils.duration(slot)
-
-    const add = start.diff(now).as('day') % 7
-
-      return start
-  }
-
-  static nextStreamEnd(slot: Stream, now?: DateTime) {
-    return YogsStreamUtils.nextStream(slot, now).plus(YogsStreamUtils.duration(slot))
+    return DateTime.fromJSDate(slot.end, {
+      zone: 'Europe/London'
+    })
   }
 
   static isLive(slot: Stream, now?: DateTime) {
     if (!now) {
       now = DateTime.now()
     }
-    const start = YogsStreamUtils.nextStream(slot, now)
-    const end = YogsStreamUtils.nextStreamEnd(slot, now)
+    const start = YogsStreamUtils.start(slot)
+    const end = YogsStreamUtils.end(slot)
     return now > start && now < end
   }
 
   static isOver(slot: Stream, now?: DateTime) {
     now ??= DateTime.now()
-    return now > YogsStreamUtils.nextStream(slot, now).plus(YogsStreamUtils.duration(slot))
+    const end = YogsStreamUtils.end(slot)
+    return now > end;
   }
 
   static isBefore(slot: Stream, now?: DateTime) {
     now ??= DateTime.now()
-    return now < YogsStreamUtils.nextStream(slot, now)
+    const start = YogsStreamUtils.start(slot)
+    return now < start;
   }
 }
